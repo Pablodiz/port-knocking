19c2d85d10b10714e4fb55815d766530b64ca634
Pablo <p.dcruz@udc.es>
2024-08-24 16:59:28 +0200

    [DOCKER] Terminado el contenedor receptor

--- docker/script.sh
+++ docker/script.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# Bloquear todas las conexiones al puerto 22 por defecto
+iptables -A INPUT -p tcp --dport 22 -j DROP
+
 # Hay que arrancar los servicios de esta forma porque si no se intentan arrancar antes de que la máquina esté terminada, 
 # por lo que no se mantendrán en ejecución cuando esté iniciado. 
 service ssh start
-service knockd start
-service netfilter-persistent start
+knockd -D
 
 # Ejecuto bash para que el docker no se apague al acabar el resto de procesos
-exec /bin/bash
-
+sleep infinity
