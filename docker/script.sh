c604fa8d3de76c82742878d2abd33ae8da0b2a27
Pablo <p.dcruz@udc.es>
2024-08-26 15:40:41 +0200

    [DOCKER] Drop por defecto en vez de filtrar puerto 22

--- docker/script.sh
+++ docker/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Bloquear todas las conexiones al puerto 22 por defecto
-iptables -A INPUT -p tcp --dport 22 -j DROP
+iptables -P INPUT DROP 
 
 # Hay que arrancar los servicios de esta forma porque si no se intentan arrancar antes de que la máquina esté terminada, 
 # por lo que no se mantendrán en ejecución cuando esté iniciado. 
